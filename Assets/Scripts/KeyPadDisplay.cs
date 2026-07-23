8a49befd31f86729aeabfc576389f437d22f4ee7
Finn Geraghty <finngeraghty2008@gmail.com>
2026-07-23 16:29:45 +0100

    started making buts work

--- Assets/Scripts/KeyPadDisplay.cs
+++ Assets/Scripts/KeyPadDisplay.cs
@@ -1,9 +1,18 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using TMPro;
 
 public class KeyPadDisplay : MonoBehaviour
 {
+
+    public TextMeshProUGUI text;
+    void Start()
+    { 
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
     public void digit1()
     {
-
+        text.text += 1.ToString();
     }
 }
