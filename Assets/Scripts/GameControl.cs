a2ce0f2f2b698a96eae3c2adbec3bd36868eb37f
Finn Geraghty <finngeraghty2008@gmail.com>
2026-07-23 16:33:54 +0100

    test

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -22,13 +22,23 @@ public class GameControl : MonoBehaviour
         
     }
 
+    public static int SumOfDigits(int num){
+        return x%10+x/10%10+x/100%10+x/1000%10+x/10000%10+x/10000%10;
+    }
+
     public static Func<int,bool> DivisbleBy(int Divisor){
         Func<int,bool> div;
         return div = x => x % Divisor == 0;
     }
 
-    public static Func<int,bool> SumTo(int Sum){
+    public static Func<int,bool> SumInRange(int SumLow, int SumHigh){
         Func<int,bool> sum;
-        return sum = x => x%10+x/10%10+x/100%10+x/1000%10+x/10000%10+x/10000%10 == Sum;
+        return sum = x => SumOfDigits(x) >= SumLow && SumOfDigits(x) <= SumHigh;
+    }
+
+    public static bool SumIsPrime(int num){
+        int sum = SumOfDigits(num);
+        List<int> squares = new List<int>{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59};
+        return squares.Contains(sum);
     }
 }
--- Assets/Scripts/KeyPadDisplay.cs
+++ Assets/Scripts/KeyPadDisplay.cs
@@ -15,4 +15,29 @@ public class KeyPadDisplay : MonoBehaviour
     {
         text.text += 1.ToString();
     }
+    
+    public void digit2()
+    {
+        text.text += 2.ToString();
+    }
+    public void digit3()
+    {
+        text.text += 3.ToString();
+    }
+    public void digit4()
+    {
+        text.text += 4.ToString();
+    }
+    public void digit5()
+    {
+        text.text += 5.ToString();
+    }
+    public void digit6()
+    {
+        text.text += 6.ToString();
+    }
+    public void digit7()
+    {
+        text.text += 7.ToString();
+    }
 }
