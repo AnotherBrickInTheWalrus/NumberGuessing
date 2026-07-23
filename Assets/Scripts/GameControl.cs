9f8da5583800b3feadb66491d8de359cf315b3eb
AnotherBrickInTheWalrus <theisaacfrancis@gmail.com>
2026-07-23 14:12:56 +0100

    Game Controller Added

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class GameControl : MonoBehaviour
 {
+    public int GuessesRemaining;
+    public int NumOfDigits;
+    public int NumOfRules;
+    public List <int> GuessedNumbers;
+    public List <List<bool>> GuessResults;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GuessedNumbers = new List <int> {};
     }
 
     // Update is called once per frame
