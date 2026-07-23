96614fd0d2a1ba3ec4fcd3823b3199fee847d497
AnotherBrickInTheWalrus <theisaacfrancis@gmail.com>
2026-07-23 13:38:01 +0100

    Game Controller Added

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameControl : MonoBehaviour
 {
-    public float Number;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Number = 67;
+        
     }
 
     // Update is called once per frame
