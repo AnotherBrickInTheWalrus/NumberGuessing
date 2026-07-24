3048b9d8b8b4509411d98da3dceb772178c0b0cf
Finn Geraghty <finngeraghty2008@gmail.com>
2026-07-24 12:55:40 +0100

    created prefab

---
+++ Assets/Scripts/DisplayPlacer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DisplayPlacer : MonoBehaviour
+{
+    private float XMidCoord = 10.0f;
+    private float YMidCoord = 10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
