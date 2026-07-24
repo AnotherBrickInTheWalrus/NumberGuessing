d8bc6c10bdbb389ed2a525b2d1bd799dd958ff74
Finn Geraghty <finngeraghty2008@gmail.com>
2026-07-24 14:13:32 +0100

    here ya go

--- Assets/Scripts/DisplayPlacer.cs
+++ Assets/Scripts/DisplayPlacer.cs
@@ -4,9 +4,13 @@ public class DisplayPlacer : MonoBehaviour
 {
     private float XMidCoord = 10.0f;
     private float YMidCoord = 10.0f;
+    private GameControl Control;
+    int NumDigits
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Control = GameObject.Find("GameController").GetComponent<GameControl>();
+        
         
     }
 
