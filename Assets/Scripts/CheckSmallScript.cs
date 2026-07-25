00ddc1a437190d9d6de24e97fef10ef80a77b8ff
AnotherBrickInTheWalrus <theisaacfrancis@gmail.com>
2026-07-25 18:54:16 +0100

    Rules history display

---
+++ Assets/Scripts/CheckSmallScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class CheckSmallScript : MonoBehaviour
+{
+    public List<bool> result;
+    private GameControl Control;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Control = GameObject.Find("GameController").GetComponent<GameControl>();
+        result = Control.GuessResults[Control.GuessResults.Count-1];
+        Debug.Log(result.Count);
+        Debug.Log(Convert.ToInt32(gameObject.name[gameObject.name.Length-1]));
+        gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Check{result[Convert.ToInt32(gameObject.name[gameObject.name.Length-1])]}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
