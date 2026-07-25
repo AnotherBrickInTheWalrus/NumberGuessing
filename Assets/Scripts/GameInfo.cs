951982f8aba73055cb5b93d35f2d310e27c86efc
Finn Geraghty <finngeraghty2008@gmail.com>
2026-07-25 16:27:17 +0100

    main menu done 2

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 public class GameInfo : MonoBehaviour
 {
     private TextMeshProUGUI DifficultyField;
     public string Difficulty;
     private GameObject CustomDifficulty;
     private bool CurrentlyCustom = false;
+    private int NumDigits;
+    private int NumRules;
+    private int NumGuesses;
     
     void Awake()
     {
@@ -43,6 +49,48 @@ public class GameInfo : MonoBehaviour
             CurrentlyCustom = false;
         }
     }
+
+    public void loadNext()
+    {
+        Difficulty = DifficultyField.text;
+        NumGuesses = 50;
+        if (Difficulty == "Easy")
+        {
+            NumDigits = 4;
+            NumRules = 2;
+        }
+        else if (Difficulty == "Medium")
+        {
+            NumDigits = 5;
+            NumRules = 3;
+        }
+        else if (Difficulty == "Hard")
+        {
+            NumDigits = 5;
+            NumRules = 4;
+        }
+        else if (Difficulty == "Extreme")
+        {
+            NumDigits = 6;
+            NumRules = 5;
+        }
+        else if (Difficulty == "Custom")
+        {
+            NumDigits = Convert.ToInt32(GameObject.Find("CustomDigits").GetComponent<TextMeshProUGUI>().text);
+            NumRules = Convert.ToInt32(GameObject.Find("CustomRules").GetComponent<TextMeshProUGUI>().text);
+            NumGuesses = Convert.ToInt32(GameObject.Find("CustomGuesses").GetComponent<TextMeshProUGUI>().text);
+        }
+
+        SceneManager.LoadScene("1Rule");
+
+    }
+
+    public List<int> GetParameters()
+    {
+        return new List<int> { NumDigits, NumRules, NumGuesses };
+    }
+    
+    
     
     
 }
