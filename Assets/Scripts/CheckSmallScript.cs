c4c48e812d9971d31cd9cf314730dd60812e24b0
EWrig <euantw@icloud.com>
2026-07-25 20:13:17 +0100

    Rules functions updated

--- Assets/Scripts/CheckSmallScript.cs
+++ Assets/Scripts/CheckSmallScript.cs
@@ -13,8 +13,8 @@ public class CheckSmallScript : MonoBehaviour
         Control = GameObject.Find("GameController").GetComponent<GameControl>();
         result = Control.GuessResults[Control.GuessResults.Count-1];
         Debug.Log(result.Count);
-        Debug.Log(Convert.ToInt32(gameObject.name[gameObject.name.Length-1]));
-        gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Check{result[Convert.ToInt32(gameObject.name[gameObject.name.Length-1])]}");
+        Debug.Log(Convert.ToInt32((gameObject.name[gameObject.name.Length-1]).ToString()));
+        gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Check{result[Convert.ToInt32((gameObject.name[gameObject.name.Length-1]).ToString())]}");
     }
 
     // Update is called once per frame
